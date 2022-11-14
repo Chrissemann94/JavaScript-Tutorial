be7b49263cc2eff39b586cb73af8640c087ad452
SageDawn <oiderdaun@gmx.at>
2022-11-14 02:15:08 +0100

    Debugged main.js

--- liqui-planner/src/js/main.js
+++ liqui-planner/src/js/main.js
@@ -3,7 +3,7 @@
 const haushaltsbuch = {
 
     eintraege: [],
-
+    fehler: [],
     gesamtbilanz: new Map(),
     
 
@@ -11,32 +11,117 @@ const haushaltsbuch = {
     EintragErfassen() {       
             let neuerEintrag = new Map();
 
-            neuerEintrag.set("Titel", prompt("Titel:", "Miete")).trim();
-            neuerEintrag.set("Typ", prompt("Typ (Einnahme oder Ausgabe):", "Einnahme")).trim();
-            neuerEintrag.set("Betrag", this.BetragVerarbeiten(parseInt(prompt("Betrag (in Euro, ohne €-Zeichen):")))).trim();
-            neuerEintrag.set("Datum", new Date(prompt("Datum:", "jjjj-mm-tt"))).trim();
+            neuerEintrag.set("Titel", this.TitelVerarbeiten(prompt("Titel:", "Miete")));
+            neuerEintrag.set("Typ", this.TypVerarbeiten(prompt("Typ (Einnahme oder Ausgabe):", "Einnahme")));
+            neuerEintrag.set("Betrag", this.BetragVerarbeiten(prompt("Betrag (in Euro, ohne €-Zeichen):")));
+            neuerEintrag.set("Datum", this.DatumVerarbeiten(prompt("Datum:", "jjjj-mm-tt")));
             neuerEintrag.set("timestamp", Date.now());
+            if (this.fehler.length === 0)
+            {
             this.eintraege.push(neuerEintrag);
-            },          
-     
+            }
+            else {
+                console.log("Folgende Fehler wurden gefunden: ")
+               this.fehler.forEach(function(fehler) {
+                console.log(fehler);
+               }); 
+            }
+        },    
+
+     /* Eingabedaten validieren und bearbeiten */
+
+    TitelValidieren(titel) {
+        if (titel !== "")     // Regex für beliebige Zahl mit oder ohne zwei Kommastellen
+         {
+            return true;
+         }
+        else 
+         {
+            return false;
+         }
+    },   
+            
+  
+    TitelVerarbeiten(titel) {
+        titel = titel.trim();
+        if(this.TitelValidieren(titel)) 
+        {
+            return titel;
+        }
+        else {
+            this.fehler.push(`Kein Titel angegeben.`);
+            return false;
+        }       
+    },
+
+    TypValidieren(typ) {
+        if (typ !== /^(?:einnahme|ausgabe)$/)     // Regex für beliebige Zahl mit oder ohne zwei Kommastellen
+         {
+            return true;
+         }
+        else 
+         {
+            return false;
+         }
+    },   
+            
+  
+    TypVerarbeiten(typ) {
+        typ = typ.trim().toLowerCase();
+        if(this.TypValidieren(typ)) 
+        {
+            return typ;
+        }
+        else {
+            this.fehler.push(`Ungültiger Typ angegeben: ${typ}`);
+            return false;
+        }       
+    },
+
+
     BetragValidieren(betrag) {
-        if (betrag.math(/^\d+(?:(?:,|\.)\d\d?)?$/) != null)     // Regex für beliebige Zahl mit oder ohne zwei Kommastellen
+        if (betrag.match(/^\d+(?:(?:,|\.)\d\d?)?$/) !== null)     // Regex für beliebige Zahl mit oder ohne zwei Kommastellen
         {
             return true;
         }
         else 
         {
-            return false
+            return false;
         }
-    }        
+    },        
     
     BetragVerarbeiten(betrag) {
+        betrag = betrag.trim();
         if(this.BetragValidieren(betrag)) 
         {
             return parseFloat(betrag.replace(",", ".")) * 100;
         }
         else {
-            console.log(`Ungültiger Betrag: ${betrag} €`)
+            this.fehler.push(`Ungültiger Betrag: ${betrag} €`);
+            return false;
+        }       
+    },
+
+    DatumValidieren(datum) {
+        if (datum.match(/^\d{4}-\d{2}-\d{2}$/) !== null)     // Regex für Datum (ausbaufähig)
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    },        
+    
+    DatumVerarbeiten(datum) {
+        datum = datum.trim();
+        if(this.DatumValidieren(datum)) 
+        {
+            return new Date(`${datum} 00:00:00`);
+        }
+        else {
+            this.fehler.push(`Ungültiges Datum: "${datum}"`);
+            return false;
         }       
     },
 
@@ -81,11 +166,11 @@ const haushaltsbuch = {
 
         this.eintraege.forEach(function(eintrag) {
             switch(eintrag.get("Typ")) {
-                case "Einnahme":
+                case "einnahme":
                     neueGesamtbilanz.set("Einnahmen", neueGesamtbilanz.get("Einnahmen") + eintrag.get("Betrag"));
                     neueGesamtbilanz.set("Bilanz", neueGesamtbilanz.get("Bilanz") + eintrag.get("Betrag"));
                     break;
-                case "Ausgabe":
+                case "ausgabe":
                     neueGesamtbilanz.set("Ausgaben", neueGesamtbilanz.get("Ausgaben") + eintrag.get("Betrag"));
                     neueGesamtbilanz.set("Bilanz", neueGesamtbilanz.get("Bilanz") - eintrag.get("Betrag"));
                     break;
@@ -101,7 +186,7 @@ const haushaltsbuch = {
     // // Gesamtbilanz ausgeben
     GesamtbilanzAusgeben() {
 
-    console.log(`Einnahmen: ${(this.gesamtbilanz.get("Einnahmen" / 100)).toFixed(2)} €\n`
+    console.log(`Einnahmen: ${(this.gesamtbilanz.get("Einnahmen") / 100).toFixed(2)} €\n`
                 + `Ausgaben: ${(this.gesamtbilanz.get("Ausgaben")).toFixed(2)} €\n`
                 + `Bilanz: ${(this.gesamtbilanz.get("Bilanz")).toFixed(2)} €\n`
                 + `Bilanz ist positiv: ${this.gesamtbilanz.bilanz >= 0}`);
@@ -113,11 +198,16 @@ const haushaltsbuch = {
 
         while(weitererEintrag) {
             this.EintragErfassen();
-            this.EintraegeSortieren();
+            if(this.fehler.length === 0)
+            {            this.EintraegeSortieren();
             this.EintraegeAusgeben();
             this.GesamtbilanzErstellen();
             this.GesamtbilanzAusgeben();
-
+            }
+            else
+            {
+                this.fehler = [];
+            }
             weitererEintrag = confirm("Weiteren Eintrag hinzufügen?");
         }
     }
